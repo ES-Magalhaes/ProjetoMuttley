8deaf251e05d5b8a9557d0c708534271b5761eb7
ES-Magalhaes <menemes.razos25@gmail.com>
2026-04-17 21:51:38 -0300

    :sparkles: feat: Adição de atributos em Evento

--- src/main/java/com/muttley/evento/DadosEvento.java
+++ src/main/java/com/muttley/evento/DadosEvento.java
@@ -1,5 +1,9 @@
 package com.muttley.evento;
 
+import java.time.LocalDate;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
@@ -9,7 +13,9 @@ public record DadosEvento(Long id,
 
 		@NotBlank(message = "A descrição é obrigatória") String descricao,
 
-		@NotNull(message = "A pontuação é obrigatória") Integer pontos,
+		@NotBlank(message = "O local do evento é obrigatório") String local,
+
+		@NotNull(message = "A data do evento é obrigatória") @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate data,
 
 		@NotNull(message = "Selecione um organizador") Long organizadorId) {
 }
--- src/main/java/com/muttley/evento/Evento.java
+++ src/main/java/com/muttley/evento/Evento.java
@@ -1,12 +1,27 @@
 package com.muttley.evento;
 
-import jakarta.persistence.*;
-import lombok.*;
+import java.time.LocalDate;
 import java.util.List;
 
+import org.springframework.format.annotation.DateTimeFormat;
+
 import com.muttley.aluno.Aluno;
-import com.muttley.organizador.Organizador;
 import com.muttley.certificado.Certificado;
+import com.muttley.organizador.Organizador;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Getter
@@ -20,7 +35,10 @@ public class Evento {
 
 	private String tema;
 	private String descricao;
-	private Integer pontos; // Referente ao "Contabilizar Pontos" do seu desenho
+	private String local;
+
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
+	private LocalDate data;
 
 	@ManyToOne
 	@JoinColumn(name = "organizador_id")
@@ -29,6 +47,6 @@ public class Evento {
 	@ManyToMany(mappedBy = "eventos")
 	private List<Aluno> participantes;
 
-	@OneToMany(mappedBy = "evento")
+	@OneToMany(mappedBy = "evento", cascade = CascadeType.ALL, orphanRemoval = true)
 	private List<Certificado> certificados;
 }
